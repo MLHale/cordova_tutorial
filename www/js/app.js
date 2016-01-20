6b037478f98091c0a44d3e80e8ee713f84f01290
mlhale <mlhale@unomaha.edu>
2016-01-20 02:00:28 -0800

    end of module 9

--- www/js/app.js
+++ www/js/app.js
@@ -3,12 +3,12 @@
 
     /* ---------------------------------- Local Variables ---------------------------------- */
     //handlebar templates
-    var homeTpl = Handlebars.compile($("#home-tpl").html());
-    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    
     var service = new EmployeeService();
     service.initialize().done(function () {
-        console.log("Service initialized");
-        renderHomeView();
+        $('body').html(new HomeView(service).render().$el);
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
@@ -27,15 +27,5 @@
     }, false);
 
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            //This passes the employees variable returned from the service to the compiled handlebars template
-            $('.content').html(employeeListTpl(employees));
-        });
-    }
 
-    function renderHomeView() {
-        $('body').html(homeTpl());
-        $('.search-key').on('keyup', findByName);
-    }
 }());
