419825a34857483927b93e56cf848228bb35ee36
mlhale <mlhale@unomaha.edu>
2016-01-20 02:03:39 -0800

    end of module 10

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -5,10 +5,21 @@
     //handlebar templates
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
     EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
     
     var service = new EmployeeService();
     service.initialize().done(function () {
-        $('body').html(new HomeView(service).render().$el);
+      router.addRoute('', function() {
+          $('body').html(new HomeView(service).render().$el);
+      });
+
+      router.addRoute('employees/:id', function(id) {
+          service.findById(parseInt(id)).done(function(employee) {
+              $('body').html(new EmployeeView(employee).render().$el);
+          });
+      });
+
+      router.start();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
